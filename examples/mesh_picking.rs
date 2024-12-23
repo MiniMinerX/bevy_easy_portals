ba206f5f0ce5e0229bf8ad87c6759708c5ea8cd7
chompaa <antony.m.3012@gmail.com>
2024-12-22 23:12:04 -0500

    Improve picking example

--- examples/mesh_picking.rs
+++ examples/mesh_picking.rs
@@ -1,7 +1,7 @@
-use std::f32::consts::{FRAC_PI_2, FRAC_PI_4};
+use std::f32::consts::FRAC_PI_4;
 
 use bevy::{
-    color::palettes::tailwind::{AMBER_400, VIOLET_400},
+    color::palettes::tailwind::{AMBER_600, VIOLET_400},
     picking::pointer::PointerInteraction,
     prelude::*,
     render::view::RenderLayers,
@@ -21,7 +21,7 @@ fn main() {
             MeshPickingPlugin,
         ))
         .add_systems(Startup, setup)
-        .add_systems(Update, (rotate, draw_mesh_intersections))
+        .add_systems(Update, draw_mesh_intersections)
         .run();
 }
 
@@ -32,7 +32,7 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut config_store: ResMut<GizmoConfigStore>,
+    #[cfg(feature = "gizmos")] mut config_store: ResMut<GizmoConfigStore>,
 ) {
     let primary_camera = commands
         .spawn((
@@ -41,7 +41,7 @@ fn setup(
                 clear_color: ClearColorConfig::Custom(Color::BLACK),
                 ..default()
             },
-            Transform::from_xyz(-5.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
             RenderLayers::from_layers(&[0, 1]),
         ))
         .id();
@@ -57,50 +57,52 @@ fn setup(
         ..default()
     });
 
-    let idle_material = materials.add(Color::from(AMBER_400));
+    let idle_material = materials.add(Color::from(AMBER_600));
     let drag_material = materials.add(Color::from(VIOLET_400));
 
     commands
         .spawn((
             Mesh3d(meshes.add(Cuboid::default())),
             MeshMaterial3d(idle_material.clone()),
-            Transform::from_xyz(1.5, 0.0, 0.0).with_rotation(Quat::from_rotation_z(-FRAC_PI_4)),
+            Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(Quat::from_rotation_z(-FRAC_PI_4)),
         ))
         .observe(rotate_on_drag)
         .observe(update_material_on::<Pointer<DragStart>>(drag_material))
         .observe(update_material_on::<Pointer<DragEnd>>(idle_material));
 
-    let target = commands.spawn(Transform::from_xyz(1.5, 0.0, 2.0)).id();
-
+    let target = commands.spawn(Transform::from_xyz(0.0, 0.0, 2.0)).id();
     let rectangle = Rectangle::from_size(Vec2::splat(2.5));
-    let portal_transform =
-        Transform::from_xyz(-1.5, 0.0, -1.0).with_rotation(Quat::from_rotation_y(-FRAC_PI_4));
 
     let idle_material = materials.add(Color::WHITE.with_alpha(0.01));
     let hover_material = materials.add(Color::WHITE.with_alpha(0.04));
 
-    commands
-        .spawn((
-            Mesh3d(meshes.add(rectangle)),
-            portal_transform,
-            Portal::new(primary_camera, target),
-            RenderLayers::layer(1),
-            PickingBehavior {
-                should_block_lower: false,
-                is_hoverable: true,
-            },
-        ))
-        .with_children(|parent| {
-            parent
-                .spawn((
-                    Mesh3d(meshes.add(rectangle)),
-                    MeshMaterial3d(idle_material.clone()),
-                    Glass,
-                    RenderLayers::layer(1),
-                ))
-                .observe(update_material_on::<Pointer<Over>>(hover_material))
-                .observe(update_material_on::<Pointer<Out>>(idle_material));
-        });
+    for portal_transform in [
+        Transform::from_xyz(-2.5, 0.0, -1.0).with_rotation(Quat::from_rotation_y(FRAC_PI_4)),
+        Transform::from_xyz(2.5, 0.0, -1.0).with_rotation(Quat::from_rotation_y(-FRAC_PI_4)),
+    ] {
+        commands
+            .spawn((
+                Mesh3d(meshes.add(rectangle)),
+                portal_transform,
+                Portal::new(primary_camera, target),
+                RenderLayers::layer(1),
+                PickingBehavior {
+                    should_block_lower: false,
+                    is_hoverable: true,
+                },
+            ))
+            .with_children(|parent| {
+                parent
+                    .spawn((
+                        Mesh3d(meshes.add(rectangle)),
+                        MeshMaterial3d(idle_material.clone()),
+                        Glass,
+                        RenderLayers::layer(1),
+                    ))
+                    .observe(update_material_on::<Pointer<Over>>(hover_material.clone()))
+                    .observe(update_material_on::<Pointer<Out>>(idle_material.clone()));
+            });
+    }
 }
 
 fn update_material_on<E>(
@@ -132,24 +134,4 @@ fn rotate_on_drag(drag: Trigger<Pointer<Drag>>, mut transform_query: Query<&mut 
     let mut transform = transform_query.get_mut(drag.entity()).unwrap();
     transform.rotate_y(drag.delta.x * 0.02);
     transform.rotate_x(drag.delta.y * 0.02);
-}
-
-fn rotate(
-    mut positive: Local<bool>,
-    mut query: Query<&mut Transform, With<Portal>>,
-    time: Res<Time>,
-) {
-    for mut transform in &mut query {
-        let (y, _x, _z) = transform.rotation.to_euler(EulerRot::YXZ);
-
-        if y >= 0.0 {
-            *positive = false;
-        } else if y <= -FRAC_PI_2 {
-            *positive = true;
-        }
-
-        let sign = if *positive { 1.0 } else { -1.0 };
-
-        transform.rotate_y(sign * time.delta_secs() / 16.0);
-    }
 }
